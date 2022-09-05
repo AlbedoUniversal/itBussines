177e01b420840ac7bad1531ff1f7f021d63a6c08
Nikita Naumov <naumov99.rst@gmail.com>
2022-09-05 08:40:16 +0300

    fix: remove htmlWebp

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,12 +3,11 @@ import include from "gulp-file-include";
 import replace from "gulp-replace";
 import webpHtml from "gulp-webp-html";
 
-const html = () =>
+const html = () =>(
   gulp
     .src($.path.src.html)
     .pipe(include())
     .pipe(replace(/@img\//g, "images/"))
-    .pipe(webpHtml())
-    .pipe(gulp.dest($.path.dest.html));
-
+    .pipe(gulp.dest($.path.dest.html))
+)
 export default html;
