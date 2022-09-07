ac557e9192dce914f77bac3b3e925641253ca5ae
Nikita Naumov <naumov99.rst@gmail.com>
2022-09-08 00:10:46 +0300

    feat; add form

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
-import { phoneInput } from './modules/phoneinput.js';
-import { tabs } from './modules/tabs.js';
+import { phoneInput } from "./modules/phoneinput.js";
+import { tabs } from "./modules/tabs.js";
+import { postForm } from "./modules/postForm.js";
 
 phoneInput();
 tabs();
+postForm();
