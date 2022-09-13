fbe42676a5253b9b855cd62563f06d34b862ada0
Albert Rybalko <albert.rybalko@chulakov.ru>
2022-09-13 22:27:48 +0300

    feat: fucking cross

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,24 +1,27 @@
 export function tabs() {
-	const cross = document.querySelector('.whatLocalization__cross');
+	// const cross = document.querySelector('.whatLocalization__cross');
+	const crosses = document.querySelectorAll('.whatLocalization__cross');
+
 	const text = document.querySelectorAll('.whatLocalization__text');
 	const links = document.querySelectorAll('.whatLocalization__link');
-	cross.addEventListener('click', (e) => {
-		// e.preventDefault();
 
-		text.forEach((el) => {
-			el.style.animation = 'callBack 1s forwards';
+	crosses.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			text.forEach((el) => {
+				el.style.animation = 'callBack 1s forwards';
+			});
+
+			e.currentTarget.style.animation = 'dissshowcross 1s forwards';
 		});
-
-		e.currentTarget.style.animation = 'dissshowcross 1s forwards';
 	});
 
-	links.forEach((el) => {
+	links.forEach((el, idx) => {
 		el.addEventListener('click', (e) => {
 			e.preventDefault();
 			const hash = el.getAttribute('href');
 			window.location = hash;
-			cross.style.animation = 'showcross 1s forwards';
-			cross.style.animationDuration = '1.3s';
+			crosses[idx].style.animation = 'showcross 1s forwards';
+			crosses[idx].style.animationDuration = '1.3s';
 			text.forEach((el) => {
 				el.style.animationDuration = '5.3s';
 				el.style.animation = 'moveLeft forwards';
